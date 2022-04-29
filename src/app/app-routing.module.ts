d87e4f3d3c23fef4bce5d101dcbe4f7de482ed4d
Eduarda Matias <evcm2003@gmail.com>
2022-04-29 14:20:34 -0300

    adicionando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'entrar', component: EntrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
